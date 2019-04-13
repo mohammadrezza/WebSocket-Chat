25c02e69d58848a52992bd5817b0620f18cdb36b
Mohammadreza Mirhajianmoghadam <mrmirhajian1996@gmail.com>
2019-04-14 00:22:12 +0430

    stable websocket chat with username input page

--- app.py
+++ app.py
@@ -1,25 +1,30 @@
 from flask_socketio import SocketIO
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
 
-@app.route('/',methods=['GET'])
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+
+@app.route('/chatroom', methods=['POST'])
 def sessions():
-    return render_template('session.html')
+    data = request.form.to_dict()
+    username = data["username"]
+    print(username)
+    return render_template('session.html', username=username)
 
 
-def message_received():
-    print('message was received!!!')
-
-
-@socketio.on('my event')
-def handle_my_custom_event(json):
-    print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=message_received)
+@socketio.on('msg event')
+def handle_msg_event(json):
+    # socketio.send(json, broadcast=True)
+    print(json)
+    socketio.emit('msg event', json)
 
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", debug=True)
+    socketio.run(app, host="0.0.0.0", debug=True, use_reloader=True)
